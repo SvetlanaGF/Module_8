8a53e6927e21e84de2a6fe927972899a7e7b1ed0
SvetlanaGF <svgf@mail.ru>
2025-01-05 22:28:13 +0300

    исправлены ошибки

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
-const { request } = require("express");
+const express = require("express"); // Импортируем express
+const { add, subtract} = require("./math"); // Правильный путь к файлу math.js
 
-const express = request;
+const app = express();
+const PORT = 3000;
 
-const math = request('.math');
+app.get('/add', (req, res) => {
+    const { a, b } = req.query;
+    const result = add(Number(a), Number(b));
+    res.send(`Result: ${result}`);
+});
 
-console.log(math.add(2,3));
-console.log(math.subtract(5,2));
+app.get('/subtract', (req, res) => {
+    const { a, b } = req.query;
+    const result = subtract(Number(a), Number(b));
+    res.send(`Result: ${result}`);
+});
+
+// Запуск сервера на порту 3000
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
+
+
+console.log(add(2, 3)); // Выводит 5
+console.log(subtract(5, 2)); // Выводит 3
